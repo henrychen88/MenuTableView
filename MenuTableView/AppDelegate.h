a6bfd9c4752bff3524216a54a6f4f91c468f8f0a
Alex <ychen@ichint.com>
2015-02-10 19:27:52 +0800

    Initial Commit

---
+++ MenuTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MenuTableView
+//
+//  Created by Henry on 15-2-10.
+//  Copyright (c) 2015年 Henry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MenuTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MenuTableView
+//
+//  Created by Henry on 15-2-10.
+//  Copyright (c) 2015年 Henry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
